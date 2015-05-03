611402dd3dfe96f107284e30b6c833bcb7861759
Rémy <remy-ferre@laposte.net>
2015-05-03 17:52:04 +0200

    Add projects for making a reservation

---
+++ RechercheVolsHotels/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RechercheVolsHotels
+{
+    public class Class1
+    {
+    }
+}
---
+++ WSHotel/App_Code/Service.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+[WebService(Namespace = "http://tempuri.org/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+// Pour autoriser l'appel de ce service Web depuis un script à l'aide d'ASP.NET AJAX, supprimez les marques de commentaire de la ligne suivante. 
+// [System.Web.Script.Services.ScriptService]
+
+public class Service : System.Web.Services.WebService
+{
+    public Service () {
+
+        //Supprimez les marques de commentaire dans la ligne suivante si vous utilisez des composants conçus 
+        //InitializeComponent(); 
+    }
+
+    [WebMethod]
+    public string HelloWorld() {
+        return "Hello World";
+    }
+    
+}
